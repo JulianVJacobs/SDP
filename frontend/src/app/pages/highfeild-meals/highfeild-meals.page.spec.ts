c9ca981907bdb45eb2ddb1b3a7b8d350798384bf
Leseli Mothibe <senatemothibe@icloud.com>
2020-08-24 21:48:02 +0200

    Update highfeild-meals.page.spec.ts

--- frontend/src/app/pages/highfeild-meals/highfeild-meals.page.spec.ts
+++ frontend/src/app/pages/highfeild-meals/highfeild-meals.page.spec.ts
@@ -25,4 +25,72 @@ describe('HighfeildMealsPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have at least one <ion-content>', () => {
+    expect(fixture.nativeElement.querySelector('ion-content')).toBeTruthy();
+  });
+
+  it('should have at least one <ion-card>', () => {
+    expect(fixture.nativeElement.querySelector('ion-card')).toBeTruthy();
+  });
+
+  it('should have at least one <ion-button>', () => {
+    expect(fixture.nativeElement.querySelector('ion-button')).toBeTruthy();
+  });
+
+  it('should have a orderAction() function', () => {
+    expect(component.orderAction).toBeTruthy();
+  });
+
+  it('should have a reviewAction() function', () => {
+    expect(component.reviewAction).toBeTruthy();
+  });
+
+  describe('Order <ion-button>', () => {
+    // it('should have an expand property', () => {
+    //   expect(fixture.nativeElement.getElementsByTagName('ion-button')[0].expand).toBeTruthy();
+    // });
+
+    // it('should have an expand property set to block', () => {
+    //   expect(fixture.nativeElement.getElementsByTagName('ion-button')[0].expand).toBe('block');
+    // });
+
+    it('should have the text "Order"', () => {
+      expect(fixture.nativeElement.getElementsByTagName('ion-button')[0].textContent).toBe("Order");
+    });
+
+    it('should have a click event', () => {
+      expect(fixture.nativeElement.getElementsByTagName('ion-button')[0].click).toBeTruthy();
+    });
+
+    it('should call orderAction() when clicked', () => {
+      let spy = spyOn(component,'orderAction');
+      fixture.nativeElement.getElementsByTagName('ion-button')[0].click();
+      expect(component.orderAction).toHaveBeenCalled();
+    });
+  });
+
+  describe('Make Review <ion-button>', () => {
+    // it('should have an expand property', () => {
+    //   expect(fixture.nativeElement.getElementsByTagName('ion-button')[0].expand).toBeTruthy();
+    // });
+
+    // it('should have an expand property set to block', () => {
+    //   expect(fixture.nativeElement.getElementsByTagName('ion-button')[0].expand).toBe('block');
+    // });
+
+    it('should have the text "Make Review"', () => {
+      expect(fixture.nativeElement.getElementsByTagName('ion-button')[1].textContent).toBe("Make Review");
+    });
+
+    it('should have a click event', () => {
+      expect(fixture.nativeElement.getElementsByTagName('ion-button')[1].click).toBeTruthy();
+    });
+
+    it('should call reviewAction() when clicked', () => {
+      let spy = spyOn(component,'reviewAction');
+      fixture.nativeElement.getElementsByTagName('ion-button')[1].click();
+      expect(component.reviewAction).toHaveBeenCalled();
+    });
+  });
 });
