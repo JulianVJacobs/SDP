cf28a14ce0cb20771c182433972b67739a3b6555
Leseli Mothibe <senatemothibe@icloud.com>
2020-08-24 18:30:45 +0200

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,24 +1,68 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [
+const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./index/index.module').then(m => m.IndexPageModule)
+    path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full'
   },
+
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'menu',
-    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
-  }
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./pages/main/main.module').then( m => m.MainPageModule)
+  },
+  {
+    path: 'dh-staff-main',
+    loadChildren: () => import('./pages/dh-staff-main/dh-staff-main.module').then( m => m.DhStaffMainPageModule)
+  },
+  {
+    path: 'convo-meals',
+    loadChildren: () => import('./pages/convo-meals/convo-meals.module').then( m => m.ConvoMealsPageModule)
+  },
+  {
+    path: 'eoh-meals',
+    loadChildren: () => import('./pages/eoh-meals/eoh-meals.module').then( m => m.EohMealsPageModule)
+  },
+  {
+    path: 'main-meals',
+    loadChildren: () => import('./pages/main-meals/main-meals.module').then( m => m.MainMealsPageModule)
+  },
+  {
+    path: 'knocks-meals',
+    loadChildren: () => import('./pages/knocks-meals/knocks-meals.module').then( m => m.KnocksMealsPageModule)
+  },
+  {
+    path: 'jubs-meals',
+    loadChildren: () => import('./pages/jubs-meals/jubs-meals.module').then( m => m.JubsMealsPageModule)
+  },
+  {
+    path: 'highfeild-meals',
+    loadChildren: () => import('./pages/highfeild-meals/highfeild-meals.module').then( m => m.HighfeildMealsPageModule)
+  },
+  {
+    path: 'make-review',
+    loadChildren: () => import('./pages/make-review/make-review.module').then( m => m.MakeReviewPageModule)
+  },
+
 ];
+
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
