049b3e8bcc7e4dede391dbda40aeff6174d5cfee
Leseli Mothibe <senatemothibe@icloud.com>
2020-08-24 22:19:58 +0200

    Update highfeild-meals.page.spec.ts

--- frontend/src/app/pages/highfeild-meals/highfeild-meals.page.spec.ts
+++ frontend/src/app/pages/highfeild-meals/highfeild-meals.page.spec.ts
@@ -26,6 +26,14 @@ describe('HighfeildMealsPage', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should have a ngOnInit() function', () => {
+    expect(component.ngOnInit).toBeTruthy();
+  });
+
+  it('should have at least one <h1> tag', () => {
+    expect(fixture.nativeElement.querySelector('h1')).toBeTruthy();
+  });
+
   it('should have at least one <ion-content>', () => {
     expect(fixture.nativeElement.querySelector('ion-content')).toBeTruthy();
   });
@@ -44,6 +52,12 @@ describe('HighfeildMealsPage', () => {
 
   it('should have a reviewAction() function', () => {
     expect(component.reviewAction).toBeTruthy();
+  });
+
+  describe('<h1>', () => {
+    it('should have the text "Highfield" before ngOnInit() is called', () => {
+      expect(fixture.nativeElement.querySelector('h1').textContent).toBe("Highfield");
+    });
   });
 
   describe('Order <ion-button>', () => {
