fa8962b041ebf0e8303a9db50d8680852c4eda20
Julian Jacobs <julian.v.jacobs@gmail.com>
2020-08-24 17:01:55 +0200

    Revert "Merge branch 'LeseliMothibe' into Julian"
    This reverts commit 01254ffc97d00cf2d68cc2838490135526a963c7, reversing
    changes made to 51c5fbfe80fec30d29098763c03c06c3e895256d.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,68 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
-    path: '', 
-    redirectTo: 'home', 
-    pathMatch: 'full'
+    path: '',
+    loadChildren: () => import('./index/index.module').then(m => m.IndexPageModule)
   },
-
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
-  },
-  {
-    path: 'main',
-    loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
-  },
-  {
-    path: 'dh-staff-main',
-    loadChildren: () => import('./dh-staff-main/dh-staff-main.module').then( m => m.DhStaffMainPageModule)
-  },
-  {
-    path: 'convo-meals',
-    loadChildren: () => import('./convo-meals/convo-meals.module').then( m => m.ConvoMealsPageModule)
-  },
-  {
-    path: 'eoh-meals',
-    loadChildren: () => import('./eoh-meals/eoh-meals.module').then( m => m.EohMealsPageModule)
-  },
-  {
-    path: 'main-meals',
-    loadChildren: () => import('./main-meals/main-meals.module').then( m => m.MainMealsPageModule)
-  },
-  {
-    path: 'knocks-meals',
-    loadChildren: () => import('./knocks-meals/knocks-meals.module').then( m => m.KnocksMealsPageModule)
-  },
-  {
-    path: 'jubs-meals',
-    loadChildren: () => import('./jubs-meals/jubs-meals.module').then( m => m.JubsMealsPageModule)
-  },
-  {
-    path: 'highfeild-meals',
-    loadChildren: () => import('./highfeild-meals/highfeild-meals.module').then( m => m.HighfeildMealsPageModule)
-  },
-  {
-    path: 'make-review',
-    loadChildren: () => import('./make-review/make-review.module').then( m => m.MakeReviewPageModule)
-  },
-
+  }
 ];
-
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
