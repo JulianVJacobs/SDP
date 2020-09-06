3df798340771466896663e097b9826c13e946296
Julian Jacobs <julian.v.jacobs@gmail.com>
2020-09-06 17:02:42 +0200

    added code to send review

--- frontend/src/app/pages/make-review/make-review.page.ts
+++ frontend/src/app/pages/make-review/make-review.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthConstants } from 'src/app/config/auth-constants';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-make-review',
@@ -7,19 +9,49 @@ import { Router } from '@angular/router';
   styleUrls: ['./make-review.page.scss'],
 })
 export class MakeReviewPage implements OnInit {
+  public review = {
+    meta: {
+      'Dining Hall': '',
+      Meal: ''
+    },
+    content: {
+      Rating: '',
+      Date: '',
+      'Posted By': '',
+      'Time': new Number,
+      Comment: ''
+    }
+  }
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private firestore: AngularFirestore
+    ) {}
 
   ngOnInit() {
+    
   }
 
   logRatingChange(rating){
     console.log("changed rating: ",rating);
     // do your stuff
+    
   }
 
   submitReview(numRating: string){
     //some back end connecting stuff
+    var today = new Date;
+
+    this.review.content.Rating = numRating;
+    this.review.content.Time = today.getTime();
+    this.review.content.Date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    this.review.content['Posted By'] = AuthConstants.personNumber;
+
+    this.firestore.firestore.collection("Dining Halls/"+this.review.meta['Dining Hall']+"/Meals/"+this.review.meta.Meal+"/Comments").doc().set(this.review.content)
+      .catch((error) => {
+        console.dir(error);
+      });
+
     console.log(numRating)
     this.router.navigate(['home/main']);
   }
