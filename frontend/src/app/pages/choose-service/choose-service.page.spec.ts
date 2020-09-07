9ea4d296f06aa8779245411ba3e794eed65a09c2
Dudu-N <61417457+Dudu-N@users.noreply.github.com>
2020-09-07 14:15:08 +0200

    test

--- frontend/src/app/pages/choose-service/choose-service.page.spec.ts
+++ frontend/src/app/pages/choose-service/choose-service.page.spec.ts
@@ -21,4 +21,26 @@ describe('ChooseServicePage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should have an foodAction() function', () => {
+    expect(component.foodAction).toBeTruthy();
+  });
+  
+  it('should have an bookAction() function', () => {
+    expect(component.bookAction).toBeTruthy();
+  });
+  
+  it('should have at least one <h1> tag', () => {
+    expect(fixture.nativeElement.querySelector('h1')).toBeTruthy();
+  });
+  
+  it('should have at least one <ion-card> tag', () => {
+    expect(fixture.nativeElement.querySelector('ion-card')).toBeTruthy();
+  });
+  
+  describe('<h1>', () => {
+    it('should have the text "Choose a Service" before ngOnInit() is called', () => {
+      expect(fixture.nativeElement.querySelector('h1').textContent).toBe("Choose a Service");
+    });
+  });
 });
