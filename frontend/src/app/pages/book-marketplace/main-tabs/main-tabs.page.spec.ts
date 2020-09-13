412a2b9333660e9783a27db49a40a0e105359a55
Dudu-N <61417457+Dudu-N@users.noreply.github.com>
2020-09-14 00:42:42 +0200

    Test

--- frontend/src/app/pages/book-marketplace/main-tabs/main-tabs.page.spec.ts
+++ frontend/src/app/pages/book-marketplace/main-tabs/main-tabs.page.spec.ts
@@ -1,1 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { IonicModule } from '@ionic/angular';
 
+import { MainTabsPage } from './main-tabs.page';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+describe('MainTabsPage', () => {
+  let component: MainTabsPage;
+  let fixture: ComponentFixture<MainTabsPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ConvoMealsPage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [IonicModule.forRoot(),RouterTestingModule, HttpClientTestingModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ConvoMealsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
