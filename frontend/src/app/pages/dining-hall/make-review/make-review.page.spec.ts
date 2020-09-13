b2d0267dc7d905f26df895dfaccf9fd1fea506b3
Dudu-N <61417457+Dudu-N@users.noreply.github.com>
2020-09-14 00:37:43 +0200

    Test

--- frontend/src/app/pages/dining-hall/make-review/make-review.page.spec.ts
+++ frontend/src/app/pages/dining-hall/make-review/make-review.page.spec.ts
@@ -1,1 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from "@angular/router/testing";
+import { IonicModule } from '@ionic/angular';
 
+import { MakeReviewPage } from './make-review.page';
+
+describe('MakeReviewPage', () => {
+  let component: MakeReviewPage;
+  let fixture: ComponentFixture<MakeReviewPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MakeReviewPage ],
+      imports: [
+        IonicModule.forRoot(), 
+        RouterTestingModule
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MakeReviewPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
