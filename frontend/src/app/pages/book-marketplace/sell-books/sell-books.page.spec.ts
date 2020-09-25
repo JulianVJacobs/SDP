a131a2b00e24f9992675a147536085869a3d7bda
Dudu-N <61417457+Dudu-N@users.noreply.github.com>
2020-09-25 22:25:56 +0200

    test

--- frontend/src/app/pages/book-marketplace/sell-books/sell-books.page.spec.ts
+++ frontend/src/app/pages/book-marketplace/sell-books/sell-books.page.spec.ts
@@ -38,4 +38,50 @@ describe('SellBooksPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should have a ngOnInit() function', () => {
+    expect(component.ngOnInit).toBeTruthy();
+  });
+  
+  it('should have at least one <h1> tag', () => {
+    expect(fixture.nativeElement.querySelector('h1')).toBeTruthy();
+  });
+  
+  it('should have at least one <ion-content>', () => {
+    expect(fixture.nativeElement.querySelector('ion-content')).toBeTruthy();
+  });
+  
+  it('should have at least one <ion-item>', () => {
+    expect(fixture.nativeElement.querySelector('ion-item')).toBeTruthy();
+  });
+  
+  it('should have at least one <ion-list>', () => {
+    expect(fixture.nativeElement.querySelector('ion-list')).toBeTruthy();
+  });
+  
+  it('should have at least one <ion-button>', () => {
+    expect(fixture.nativeElement.querySelector('ion-button')).toBeTruthy();
+  });
+  
+  it('should have a addBook() function', () => {
+    expect(component.addBook).toBeTruthy();
+  });
+  
+  it('should have a remove() function', () => {
+    expect(component.remove).toBeTruthy();
+  });
+  
+  describe('<h1>', () => {
+    it('should have the text "Sell Books" before ngOnInit() is called', () => {
+      expect(fixture.nativeElement.querySelector('h1').textContent).toBe("Sell Books");
+    });
+  });
+  
+  describe('Add <ion-button>', () => {
+
+    it('should have the text "Add"', () => {
+      expect(fixture.nativeElement.getElementsByTagName('ion-button')[0].textContent).toBe("Add");
+    });
+    
+  });
 });
