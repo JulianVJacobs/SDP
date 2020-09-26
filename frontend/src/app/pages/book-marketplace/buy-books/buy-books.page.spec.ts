eb38b07ac17a9b97f96f9a79a54ecebf9fa054a8
Julian Jacobs <julian.v.jacobs@gmail.com>
2020-09-26 23:49:56 +0200

    Revert "Update buy-books.page.spec.ts"
    This reverts commit f71918e5a1ec78e4150b4fa8b5e6e1be9b3a6beb.

--- frontend/src/app/pages/book-marketplace/buy-books/buy-books.page.spec.ts
+++ frontend/src/app/pages/book-marketplace/buy-books/buy-books.page.spec.ts
@@ -11,7 +11,7 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
-describe('BuyBooksPage', () => {
+fdescribe('BuyBooksPage', () => {
   let component: BuyBooksPage;
   let fixture: ComponentFixture<BuyBooksPage>;
 
