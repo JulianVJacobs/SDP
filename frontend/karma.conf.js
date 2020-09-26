a840b5c7597f13da6cef023db8b8ab497e76340e
Julian Jacobs <julian.v.jacobs@gmail.com>
2020-09-26 23:49:24 +0200

    Revert "firebase"
    This reverts commit 16b1d4b106fd3370d4e6b1915de8f416137762d0.

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome','Firefox','FirefoxHeadless','ChromeHeadless'],
+    browsers: ['Chrome','Firefox','FirefoxHeadless','ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
--- frontend/src/app/pages/book-marketplace/buy-books/buy-books.page.spec.ts
+++ frontend/src/app/pages/book-marketplace/buy-books/buy-books.page.spec.ts
@@ -1,16 +1,15 @@
-import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from "@angular/router/testing";
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { BuyBooksPage } from './buy-books.page';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import * as firebase from '@firebase/rules-unit-testing';
 
 describe('BuyBooksPage', () => {
   let component: BuyBooksPage;
@@ -28,23 +27,15 @@ describe('BuyBooksPage', () => {
         AngularFireModule.initializeApp(environment.firebaseConfig),
         AngularFireStorageModule,
         AngularFirestoreModule
-      ],
-      providers: [
-        AngularFireAuth
       ]
     }).compileComponents();
 
-    firebase.initializeTestApp({
-      projectId: "greenpeace-33d5f",
-      auth: { uid: "123456", email: "123456@example.com"}
-    });
-    // firebase.
     fixture = TestBed.createComponent(BuyBooksPage);
     component = fixture.componentInstance;
-    fixture.detectChanges(); 
+    fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  xit('should create', () => {
     expect(component).toBeTruthy();
   });
   
