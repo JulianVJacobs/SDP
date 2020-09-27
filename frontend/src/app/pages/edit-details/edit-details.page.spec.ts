5ad7285f8844a153169fd2c52c5c22e56d4ada54
Dudu-N <61417457+Dudu-N@users.noreply.github.com>
2020-09-27 02:08:05 +0200

    test

--- frontend/src/app/pages/edit-details/edit-details.page.spec.ts
+++ frontend/src/app/pages/edit-details/edit-details.page.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
 
 import { EditDetailsPage } from './edit-details.page';
 
